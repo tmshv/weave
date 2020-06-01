c41718747f5a24d08f1ecc6cdd3f85553920bdef
Roman Timashev <roman@tmshv.ru>
2020-06-01 15:34:14 +0300

    styletron fixes

--- weave-app/src/pages/_document.tsx
+++ weave-app/src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { Provider as StyletronProvider } from 'styletron-react'
-import { styletron } from '../styletron'
+import { styletron, getStyletronServerStylesheets } from '../styletron'
 
 class MyDocument extends Document {
     static getInitialProps(props) {
@@ -9,7 +9,8 @@ class MyDocument extends Document {
                 <App {...props} />
             </StyletronProvider>
         ))
-        const stylesheets = styletron.getStylesheets() || []
+
+        const stylesheets = getStyletronServerStylesheets()
         return { ...page, stylesheets }
     }
 
--- weave-app/src/styletron.ts
+++ weave-app/src/styletron.ts
@@ -3,10 +3,15 @@ import { DebugEngine } from 'styletron-react'
 
 const getHydrateClass = () => document.getElementsByClassName('_styletron_hydrate_')
 
+export const getStyletronServerStylesheets = () => {
+    const s = new Server()
+    return s.getStylesheets() ?? []
+}
+
 export const styletron = typeof window === 'undefined'
     ? new Server()
     : new Client({
         hydrate: getHydrateClass(),
-    })
+    } as any)
 
 export const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine()
